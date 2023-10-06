be9804831653f0f1009fa5ab3258a1c12b044d00
Kamaz <kamaz@xsimple.org>
2023-10-06 15:01:39 +0100

    chore: preview hack real file with semicolons and double quotes

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,5 +1,5 @@
 // simple example for testing it manually from your browser.
 export default function handler(req, res) {
-  res.setPreviewData({})
-  res.end('Preview mode enabled')
+  res.setPreviewData({});
+  res.end("Preview mode enabled");
 }
