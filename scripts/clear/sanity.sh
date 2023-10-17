04918310255ee9d8a7ab76a8a6f8597ebd66a334
Kamaz <kamaz@xsimple.org>
2023-10-17 16:04:57 +0100

    fix: cmd to remove webhooks

--- scripts/clear/sanity.sh
+++ scripts/clear/sanity.sh
@@ -6,6 +6,6 @@ sanity documents query --api-version v2022-06-01  '*[_type == "product"]._id' | 
 sanity documents query --api-version v2022-06-01  '*[_type == "variant"]._id' | jq -r '.[]' | while read id; do sanity documents delete $id; done
 
 # remove webhooks
-sanity hook list | grep Name: | while read name; do sanity hook delete ${name/Name: /}; done
+sanity hook list | grep Name: | while read name; do sanity hook delete "${name/Name: /}"; done
 
 
